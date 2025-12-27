5a1cdd25fcb659d58951921671f0105a6e5172e3
dabdob-craft <dabdobcraft2@gmail.com>
2025-12-27 22:18:22 +0100

    Update LuxDecompiler.java

--- src/main/java/net/luxmcje/loom/LuxDecompiler.java
+++ src/main/java/net/luxmcje/loom/LuxDecompiler.java
@@ -1,26 +1,44 @@
 package net.luxmcje.loom;
 
-import org.jetbrains.java.decompiler.main.extern.IFernflowerPreferences;
-import org.jetbrains.java.decompiler.main.decompiler.ConsoleDecompiler;
-import org.jetbrains.java.decompiler.main.extern.IFernflowerLogger;
+import org.jetbrains.java.decompiler.main.Fernflower;
+import org.jetbrains.java.decompiler.main.decompiler.PrintStreamLogger;
+import org.jetbrains.java.decompiler.main.extern.IBytecodeProvider;
+import org.jetbrains.java.decompiler.main.extern.IResultSaver;
+import java.io.*;
 import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.Map;
-import java.io.File;
+import java.util.jar.Manifest;
 
 public class LuxDecompiler {
     public static void decompile(Path inputJar, Path outputJar) {
         Map<String, Object> options = new HashMap<>();
-        options.put(IFernflowerPreferences.INDENT_STRING, "    ");
-        options.put(IFernflowerPreferences.REMOVE_BRIDGE, "1");
+        options.put("ind", "    ");
+        options.put("rbr", "1");
 
-        IFernflowerLogger logger = new IFernflowerLogger() {
-            @Override public void writeMessage(String message, Severity severity) {}
-            @Override public void writeMessage(String message, Severity severity, Throwable t) {}
+        IBytecodeProvider provider = (externalPath, internalPath) -> {
+            try (InputStream is = new FileInputStream(externalPath)) {
+                return is.readAllBytes();
+            }
         };
 
-        ConsoleDecompiler decompiler = new ConsoleDecompiler(outputJar.getParent().toFile(), options, logger);
-        decompiler.addSource(inputJar.toFile());
-        decompiler.decompileContext();
+        IResultSaver saver = new IResultSaver() {
+            @Override public void saveClassFile(String path, String clsName, String entryName, String content, int[] mapping) {}
+            @Override public void saveFolder(String path) {}
+            @Override public void copyFile(String source, String path, String entryName) {}
+            @Override public void createArchive(String path, String archiveName, Manifest manifest) {}
+            @Override public void saveDirEntry(String path, String archiveName, String entryName) {}
+            @Override public void copyEntry(String source, String path, String archiveName, String entryName) {}
+            @Override public void saveClassEntry(String path, String archiveName, String clsName, String entryName, String content) {
+                try {
+                    File out = outputJar.toFile();
+                } catch (Exception e) { e.printStackTrace(); }
+            }
+            @Override public void closeArchive(String path, String archiveName) {}
+        };
+
+        Fernflower engine = new Fernflower(provider, saver, options, new PrintStreamLogger(System.out));
+        engine.addSource(inputJar.toFile());
+        engine.decompileContext();
     }
 }
