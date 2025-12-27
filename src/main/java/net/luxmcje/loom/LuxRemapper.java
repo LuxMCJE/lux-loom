f9b87fbb7d5808b4a2f31ce1007b8f8f0c14c198
dabdob-craft <dabdobcraft2@gmail.com>
2025-12-27 20:35:44 +0100

    Create LuxRemapper.java

---
+++ src/main/java/net/luxmcje/loom/LuxRemapper.java
@@ -0,0 +1,28 @@
+package net.luxmcje.loom;
+
+import net.fabricmc.tinyremapper.OutputConsumerPath;
+import net.fabricmc.tinyremapper.TinyRemapper;
+import net.fabricmc.tinyremapper.TinyUtils;
+import java.io.File;
+import java.nio.file.Path;
+
+public class LuxRemapper {
+
+    public static void remap(Path inputJar, Path outputJar, Path mappingFile) {
+        // 1. إعداد أداة التحويل (Remapper) باستخدام قاموس Lux (Tiny)
+        TinyRemapper remapper = TinyRemapper.newRemapper()
+                .withMappings(TinyUtils.createTinyMappingProvider(mappingFile, "official", "named"))
+                .build();
+
+        try (OutputConsumerPath outputConsumer = new OutputConsumerPath.Builder(outputJar).build {
+            remapper.readInputs(inputJar);
+            
+            remapper.apply(outputConsumer);
+            remapper.finish();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            remapper.finish();
+        }
+    }
+}
