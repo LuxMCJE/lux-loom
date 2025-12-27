fb4e62a656d2ff9f3718094a129123e692668e9a
dabdob-craft <dabdobcraft2@gmail.com>
2025-12-27 21:43:26 +0100

    Update LuxDecompiler.java

--- src/main/java/net/luxmcje/loom/LuxDecompiler.java
+++ src/main/java/net/luxmcje/loom/LuxDecompiler.java
@@ -2,25 +2,20 @@ package net.luxmcje.loom;
 
 import org.jetbrains.java.decompiler.main.extern.IFernflowerPreferences;
 import org.jetbrains.java.decompiler.main.decompiler.ConsoleDecompiler;
-import java.io.File;
 import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.Map;
 
 public class LuxDecompiler {
-
     public static void decompile(Path inputJar, Path outputJar) {
         Map<String, Object> options = new HashMap<>();
         options.put(IFernflowerPreferences.INDENT_STRING, "    ");
         options.put(IFernflowerPreferences.REMOVE_BRIDGE, "1");
-        
-        File outputDir = outputJar.getParent().toFile();
-        ConsoleDecompiler decompiler = new ConsoleDecompiler(outputDir, options);
-        
+        options.put(IFernflowerPreferences.ASCII_STRING_CHARACTERS, "1");
+
+        ConsoleDecompiler decompiler = new ConsoleDecompiler(outputJar.getParent().toFile(), options);
         decompiler.addSource(inputJar.toFile());
         
         decompiler.decompileContext();
-        
-        System.out.println("[LuxLoom] Decompilation complete: " + outputJar.getFileName());
     }
 }
