82d72e3799debae49462459a809fbd5f65a6fcb6
dabdob-craft <dabdobcraft2@gmail.com>
2025-12-27 21:09:32 +0100

    Update LuxRemapper.java

--- src/main/java/net/luxmcje/loom/LuxRemapper.java
+++ src/main/java/net/luxmcje/loom/LuxRemapper.java
@@ -1,27 +1,52 @@
 package net.luxmcje.loom;
 
-import net.fabricmc.tinyremapper.OutputConsumerPath;
-import net.fabricmc.tinyremapper.TinyRemapper;
-import net.fabricmc.tinyremapper.TinyUtils;
-import java.io.File;
-import java.nio.file.Path;
+import org.objectweb.asm.*;
+import org.objectweb.asm.commons.ClassRemapper;
+import org.objectweb.asm.commons.SimpleRemapper;
+
+import java.io.*;
+import java.util.*;
+import java.util.jar.*;
 
 public class LuxRemapper {
+    private final Map<String, String> mappingMap = new HashMap<>();
 
-    public static void remap(Path inputJar, Path outputJar, Path mappingFile) {
-        TinyRemapper remapper = TinyRemapper.newRemapper()
-                .withMappings(TinyUtils.createTinyMappingProvider(mappingFile, "official", "named"))
-                .build();
+    public void loadTinyMappings(File tinyFile) throws IOException {
+        List<String> lines = Files.readAllLines(tinyFile.toPath());
+        for (String line : lines) {
+            String[] parts = line.split("\t");
+            if (parts[0].equals("CLASS")) {
+                mappingMap.put(parts[1], parts[3]);
+            }
+        }
+    }
 
-        try (OutputConsumerPath outputConsumer = new OutputConsumerPath.Builder(outputJar).build {
-            remapper.readInputs(inputJar);
+    public void remapJar(File inputJar, File outputJar) throws IOException {
+        SimpleRemapper remapper = new SimpleRemapper(mappingMap);
+        
+        try (JarFile jarFile = new JarFile(inputJar);
+             JarOutputStream jos = new JarOutputStream(new FileOutputStream(outputJar))) {
             
-            remapper.apply(outputConsumer);
-            remapper.finish();
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            remapper.finish();
+            Enumeration<JarEntry> entries = jarFile.entries();
+            while (entries.hasMoreElements()) {
+                JarEntry entry = entries.nextElement();
+                byte[] bytes = jarFile.getInputStream(entry).readAllBytes();
+
+                if (entry.getName().endsWith(".class")) {
+                    ClassReader reader = new ClassReader(bytes);
+                    ClassWriter writer = new ClassWriter(0);
+                    ClassVisitor cv = new ClassRemapper(writer, remapper);
+                    reader.accept(cv, 0);
+                    
+                    String newName = mappingMap.getOrDefault(entry.getName().replace(".class", ""), 
+                                                            entry.getName().replace(".class", "")) + ".class";
+                    jos.putNextEntry(new JarEntry(newName));
+                    jos.write(writer.toByteArray());
+                } else {
+                    jos.putNextEntry(new JarEntry(entry.getName()));
+                    jos.write(bytes);
+                }
+            }
         }
     }
 }
